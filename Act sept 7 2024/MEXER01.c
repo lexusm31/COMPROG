37eb2a2f793567f111d134d6b730c05cda595dce
LEXUS <180945520+lexusm31@users.noreply.github.com>
2024-10-03 23:17:22 +0800

    MEXER01.c

--- Act sept 7 2024/MEXER01.c
+++ Act sept 7 2024/MEXER01.c
@@ -8,16 +8,13 @@ int main() {
   scanf("%d", &distance);
 
   if (distance <= 300) {
+      printf("P %.2f (The fare is P %.2f for the first 300 meters)\n", fare, fare);
   } else {
     extra_distance = distance - 300; 
   }
   if (extra_distance > 0) {
     increments = extra_distance / 200; 
     fare += increments * 2.00; 
-  }
-  if (distance <= 300) {
-    printf("P %.2f (The fare is P %.2f for the first 300 meters)\n", fare, fare);
-  } else {
     printf("P %.2f (The first 300 meters cost P 20.00, then %d meters is charged as %d increment(s) of 200 meters)\n", fare, extra_distance, increments);
   }
   return 0;
