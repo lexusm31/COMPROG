c3f27d713cbf74c2ca21e57cf1575f04cc892e85
LEXUS <180945520+lexusm31@users.noreply.github.com>
2024-10-04 21:05:11 +0800

    MEXER04.c

--- Act sept 7 2024/MEXER04.c
+++ Act sept 7 2024/MEXER04.c
@@ -6,15 +6,15 @@ int main() {
     printf("Enter three integers: \n");
     scanf("%d %d %d", &num1, &num2, &num3);
 
-    if (num1 > num2)
+    if (num1 > num2) {
         largest = num1;
-    else
+    } else {
         largest = num2;
-
-    if (num3 > largest)
+    }
+    if (num3 > largest) {
         largest = num3;
-
-    printf("The largest number is: %d\n", largest);
-
+    }
+    printf("The largest number is: %d\n", largest);  
+    
     return 0;
 }
