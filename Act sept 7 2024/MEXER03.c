f1157575af9681130ba8f3334a3809112fae69df
LEXUS <180945520+lexusm31@users.noreply.github.com>
2024-10-04 20:51:32 +0800

    MEXER03.c

--- Act sept 7 2024/MEXER03.c
+++ Act sept 7 2024/MEXER03.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
 
 int main() {
-   
- double consumption, excess_consumption, final_bill;
- const double rate_per_kw = 50.00;
- const double adjustment_tax_rate = 0.085;
- const double threshold = 25.00;
-
- printf("Enter the total kilowatt-hour consumption: ");
- scanf("%lf", &consumption);
-
+    float consumption, excess, bill, final_bill;
+    float base = 25;
+    float rate = 50;
+    float tax = 0.085;
     
- if (consumption > threshold) {
-     excess_consumption = consumption - threshold;
- } else {
-     excess_consumption = 0;
-   }
+    printf("Enter kilowatt-hour consumption: ");
+    scanf("%f", &consumption);
 
-  
- final_bill = excess_consumption * rate_per_kw;
-  final_bill += final_bill * adjustment_tax_rate;
-   
-  printf("\nTotal Consumption: %.2f kWh", consumption);
-  printf("\nExcess Consumption: %.2f kWh", excess_consumption);
-  printf("\nFinal Electric Bill: P%.2f\n", final_bill);
+    if (consumption > base) {
+        excess = consumption - base;
+        bill = excess * rate;
+    }
 
-   return 0;
+    final_bill = bill + (bill * tax);
+
+    printf("Your consumption: %.2f\n", consumption);
+    printf("Excess: %.2f\n", excess);
+    printf("Total bill: %.2f\n", final_bill);
+
+    return 0;
 }
 
